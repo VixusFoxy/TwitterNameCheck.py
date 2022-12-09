d6f7f2dc58873b3354398b7222030041fda0850b
VixusFoxy <vixusfoxy@gmail.com>
2022-12-09 07:39:11 -0700

    Update TwitterNameCheck.py

--- TwitterNameCheck.py
+++ TwitterNameCheck.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 import time
 
 # Replace this with your actual access token - You can get one from https://developer.twitter.com
@@ -9,6 +10,8 @@ username = input("Enter a username to look up: ")
 seconds = 5
 
 while True:
+  # generate a timestamp to use in the response message
+  current_time = datetime.datetime.now()
   # use 'requests' library to make the request and see if the name is available
   resp = requests.get(
     f"https://api.twitter.com/2/users/by/username/{username}",
@@ -18,14 +21,13 @@ while True:
   # Check the response to see if it contains "Could not find user with username" or not
   if "Could not find user with username" in resp.text:
     # If the message is in the response, the username is available and we should print that in bright green
-    print("\033[32mAVAILABLE!\033[0m")
+    print(current_time.strftime("%H:%M:%S") + ": \033[32mAVAILABLE!\033[0m")
   else:
     # If the message is not in the response, the username is unavailable. Boo.
-    print("\033[31mUnavailable\033[0m")
+    print(current_time.strftime("%H:%M:%S") + ": \033[31mUnavailable\033[0m")
 
     # Uncomment the line below to print the full response from the API - this would be for debugging purposes.
     # print(resp.json())
 
-    # We want to run this on a loop of x seconds
-    time.sleep(5)
-
+  # We want to run this on a loop of x seconds
+  time.sleep(5)
